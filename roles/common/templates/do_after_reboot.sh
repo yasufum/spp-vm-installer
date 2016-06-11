591442f586557bf1c3e9cfedca0592b0edbcc910
yasufumi ogawa <ogawa.yasufumi@lab.ntt.co.jp>
2016-06-11 15:47:19 +0900

    clean common/*

--- roles/common/templates/do_after_reboot.sh
+++ roles/common/templates/do_after_reboot.sh
@@ -5,13 +5,13 @@ echo "Setup port for dpdk."
 # modprobe
 echo sudo modprobe uio_pci_generic 
 sudo modprobe uio_pci_generic 
-echo sudo modprobe vfio-pci
-sudo modprobe vfio-pci
+#echo sudo modprobe vfio-pci
+#sudo modprobe vfio-pci
 
 # setup vars for nic bind.
-script=${RTE_SDK}/tools/dpdk_nic_bind.py
-opt="--bind=uio_pci_generic"
-
+#script=${RTE_SDK}/tools/dpdk_nic_bind.py
+#opt="--bind=uio_pci_generic"
+#
 #for eth in eth2 eth3 eth4 eth5
 #do
 #  sudo ifconfig ${eth} down
