2281a15916c0f49b7703360b84b3f0e71cfc8068
Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>
2017-07-21 15:53:15 +0900

    Add start_spp_vm.sh
    start_spp_vm.sh is a script for running sec on the VM.
    
    Signed-off-by: Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>

---
+++ roles/common/templates/start_spp_vm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SEC_ID=$1
+
+sudo $(dirname $0)/src/vm/src/vm/x86_64-ivshmem-linuxapp-gcc/spp_vm \
+  -c 0x03 \
+  -n 4 \
+  --proc-type=primary \
+  -- \
+  -p 0x01 \
+  -n ${SEC_ID} \
+  -s 192.168.122.1:6666
